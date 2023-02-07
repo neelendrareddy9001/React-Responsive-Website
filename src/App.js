65c6602debe3801ee1e5a18c14d4496e348efea3
neelendrareddy90001 <neelendrareddy123@gmail.com>
2023-02-07 22:13:12 +0100

    React-Responsive-Website

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Header from './components/home/header/Header';
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomePage from './components/pageContent/HomePage';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Header/>
+        <Switch>
+          <Route path='/' exact component={HomePage}/>
+        </Switch>
+      </Router>
+    </>
+  );
+}
+
+export default App;
