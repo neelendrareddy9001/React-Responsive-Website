ddecc1aa382f90ecd934378133093031773650cd
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-09-17 23:56:29 +0200

    About section updated with styles

--- src/components/pages/HomePages.js
+++ src/components/pages/HomePages.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Home from "../home/homes/Home";
+import Branding from "../home/Branding";
+import About from "../home/about/About";
 
 const HomePages = () => {
   return (
     <div>
       <Home />
+      <Branding />
+      <About />
     </div>
   );
 };
