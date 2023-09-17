282a8be08d6be457f631a7c2a5f5afed337488c8
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-09-18 00:21:50 +0200

    service section with images and styles added

---
+++ src/components/home/services/Service.js
@@ -0,0 +1,37 @@
+import React from "react";
+import ServiceData from "./ServiceData";
+
+const Service = () => {
+  return (
+    <>
+      <section className="Services topMargin">
+        <div className="container">
+          <div className="heading">
+            <h3>SERVICES</h3>
+            <h1>Interactive Serices Offered Me</h1>
+          </div>
+
+          <div className="contain grid topMargin">
+            {ServiceData.map((vale) => {
+              return (
+                <>
+                  <div className="box">
+                    <div className="img">
+                      <img src={vale.cover} alt="" />
+                    </div>
+                    <div className="text">
+                      <h2>{vale.title}</h2>
+                      <p>{vale.desc}</p>
+                    </div>
+                  </div>
+                </>
+              );
+            })}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Service;
--- src/components/pages/HomePages.js
+++ src/components/pages/HomePages.js
@@ -2,6 +2,7 @@ import React from "react";
 import Home from "../home/homes/Home";
 import Branding from "../home/Branding";
 import About from "../home/about/About";
+import Service from "../home/services/Service";
 
 const HomePages = () => {
   return (
@@ -9,6 +10,7 @@ const HomePages = () => {
       <Home />
       <Branding />
       <About />
+      <Service />
     </div>
   );
 };
